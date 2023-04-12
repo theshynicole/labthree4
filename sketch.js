cd8bb162b5104142a465be18b88169fdbcd89cfb
theshynicole <99832261+theshynicole@users.noreply.github.com>
2023-04-11 23:02:48 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,11 @@
+let ele;
+function setup() {
+  ele = createAudio('Assignment 4 audio.mp3');
+
+  // here we set the element to autoplay
+  // The element will play as soon
+  // as it is able to do so.
+  ele.autoplay(true);
+}
+// Audio is from a project that I did in which I recorded water
+//link to reference: 
